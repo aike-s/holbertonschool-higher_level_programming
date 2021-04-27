da224a7388397dc886339342ec2c6be08ec2ff16
aike-s <sagrilo804@gmail.com>
2021-04-27 16:04:18 -0500

    [delete] innecesary space

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,7 @@ else:
 last = positive % 10
 if number < 0 and last != 0:
     print('Last digit of', number, 'is -{:d}'.format(last),
-          ' and is less than 6 and not 0')
+          'and is less than 6 and not 0')
 else:
     print('Last digit of', number, 'is', last, end='')
     if last > 5:
