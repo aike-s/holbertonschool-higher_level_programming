388454ce33023f053aba5ab81a556bd4b224cce2
aike-s <sagrilo804@gmail.com>
2021-04-27 17:44:16 -0500

    [remove] innecesary conditionals

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 for num in range(0, 99):
-    if num < 99:
-        print("{:02d}, ".format(num), end='')
+    print("{:02d}, ".format(num), end='')
 num += 1
 print("{}".format(num))
