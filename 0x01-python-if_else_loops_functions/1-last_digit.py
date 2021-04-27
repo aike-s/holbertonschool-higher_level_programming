6a9cd6f54c3b85ce718c4f8768935dc90f1f8da2
aike-s <sagrilo804@gmail.com>
2021-04-27 14:36:26 -0500

    [fix] pep8 statements and conditionals

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = number % 10
-print('Last digit of', number, 'is', last, end = '')
+if number < 0:
+    positive = number * -1
+else:
+    positive = number
+last = positive % 10
+print('Last digit of', number, 'is', last, end='')
 if last > 5:
     print(' and is greater than 5')
 elif last == 0:
