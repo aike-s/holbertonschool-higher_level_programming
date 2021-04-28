4d830aa76c6411bcca83f51cc0acea554ad5de0c
aike-s <sagrilo804@gmail.com>
2021-04-28 15:57:46 -0500

    [add] python script

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+i = 0
+length = len(sys.argv) - 1
+if length == 0:
+    print("{:d} arguments.".format(length))
+else:
+    if length == 1:
+        print("{:d} argument:".format(length))
+    else:
+        print("{:d} arguments:".format(length))
+for num in range(1, length + 1):
+    i += 1
+    print("{:d}: {:s}".format(num, sys.argv[i]))
