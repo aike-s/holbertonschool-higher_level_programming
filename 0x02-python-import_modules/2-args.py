4256456887feb7bbc1c4a6efeb5b1f624cd71742
aike-s <sagrilo804@gmail.com>
2021-04-28 16:37:06 -0500

    [add] set name to main

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import sys
-i = 0
-length = len(sys.argv) - 1
-if length == 0:
-    print("{:d} arguments.".format(length))
-else:
-    if length == 1:
-        print("{:d} argument:".format(length))
+if __name__ == "__main__":
+    import sys
+    i = 0
+    length = len(sys.argv) - 1
+    if length == 0:
+        print("{:d} arguments.".format(length))
     else:
-        print("{:d} arguments:".format(length))
-for num in range(1, length + 1):
-    i += 1
-    print("{:d}: {:s}".format(num, sys.argv[i]))
+        if length == 1:
+            print("{:d} argument:".format(length))
+        else:
+            print("{:d} arguments:".format(length))
+    for num in range(1, length + 1):
+        i += 1
+        print("{:d}: {:s}".format(num, sys.argv[i]))
