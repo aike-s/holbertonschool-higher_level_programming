78f38322a959ce1d2c4c37677c7f30fe62704c48
aike-s <sagrilo804@gmail.com>
2021-04-28 20:50:59 -0500

    [fix] find function

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     for item in dir(hidden_4):
-        if item.find('_', end = 1) != -1:
+        if item.find('_', 0, 1) != -1:
             continue
         print("{}".format(item))
