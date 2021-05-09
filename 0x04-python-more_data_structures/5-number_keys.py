c3e4cbda95657a5de69dfc9320c37c85530f04f8
aike-s <sagrilo804@gmail.com>
2021-05-08 21:21:14 -0500

    [try]

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
+    if len(a_dictionary) == 0:
+        return 0
     for num, keys in enumerate(a_dictionary):
         pass
-    if num > 0:
-        num += 1
-    return num
+    return num + 1
