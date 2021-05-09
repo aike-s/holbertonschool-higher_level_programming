0b87d3a1830c4a9b2d9d17c478e27959258fd5bb
aike-s <sagrilo804@gmail.com>
2021-05-08 19:16:53 -0500

    [fix] conditional

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return [value *= 2 for key, value in a_dictionay.items()]
+    return dict(value *= 2 for key, value in a_dictionay.items())
