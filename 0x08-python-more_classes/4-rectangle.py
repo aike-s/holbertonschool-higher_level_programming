8f315b3d814616e4f387e2cc669813da9c3ccd32
aike-s <sagrilo804@gmail.com>
2021-05-20 21:45:25 -0500

    [fix] empty line

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -3,24 +3,20 @@
 
 
 class Rectangle:
-
     """Represents a class rectangle"""
 
     def __init__(self, width=0, height=0):
-
         """Initializes rectangle"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-
         """Initializates property width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-
         """Handle except errors"""
         if type(value) is not int:
             raise TypeError('width must be an integer')
@@ -31,13 +27,11 @@ class Rectangle:
 
     @property
     def height(self):
-
         """Initializates property height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-
         """Handle except errors"""
         if type(value) is not int:
             raise TypeError('height must be an integer')
@@ -47,13 +41,11 @@ class Rectangle:
             self.__height = value
 
     def area(self):
-
         """Calculates the area of a rectangle"""
         total_area = (self.__width * self.__height)
         return total_area
 
     def perimeter(self):
-
         """Calculates the perimeter of a rectangle"""
         if self.__width == 0 or self.__height == 0:
             total_perimeter = 0
@@ -62,7 +54,6 @@ class Rectangle:
         return total_perimeter
 
     def __str__(self):
-
         """Function for print a matrix in hash"""
         if self.__width == 0 or self.__height == 0:
             return ''
@@ -77,6 +68,5 @@ class Rectangle:
         return matrix
 
     def __repr__(self):
-
         """Makes a string representation of the rectangle"""
         return f'Rectangle(width={self.__width}, height={self.__height})'
