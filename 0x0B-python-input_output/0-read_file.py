84c071eb3a0834590ef93a082f1146e738754b65
aike-s <sagrilo804@gmail.com>
2021-05-31 21:18:57 -0500

    [add] r mode

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ In this module a single function to read a file
 def read_file(filename=""):
     """ Reads a text file and prints it to stdout """
 
-    with open(filename, encoding='UTF-8') as file:
+    with open(filename, mode='r', encoding='UTF-8') as file:
         print(file.read())
