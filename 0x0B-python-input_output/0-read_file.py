6f6604f7aee7d3a48c81e41a4d42bf7648e1a886
aike-s <sagrilo804@gmail.com>
2021-06-01 17:08:20 -0500

    [fix] with conditional

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ def read_file(filename=""):
     """ Reads a text file and prints it to stdout """
 
     with open(filename, mode='r', encoding='UTF-8') as file:
-        print(file.read())
+        print(file.read(), end='')
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,11 +8,12 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
 if __name__ == "__main__":
+    filename = "add_item.json"
     try:
-        object_from_json = load_from_json_file("add_item.json")
-    except:
-        pass
+        object_from_json = load_from_json_file(filename)
+    except(FileExistsError):
         object_from_json = []
+
     for to_add in argv[1:]:
         object_from_json.append(to_add)
-    save_to_json_file(object_from_json, "add_item.json")
+    save_to_json_file(object_from_json, filename)
