3dbaaaaeeb4970bf9b03a8f6f0e7c958dcd08100
aike-s <sagrilo804@gmail.com>
2021-07-01 18:31:29 -0500

    [fix] pep8 style

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-""" Module """
-
-
 def magic_string():
-    """ Magic string """
     magic_string.counter = getattr(magic_string, 'counter', 0) + 1
     return ", ".join(["Holberton" for i in range(magic_string.counter)])
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ In this module the LockedCLass class """
 
+
 class LockedClass():
     """ restriction """
     __slots__ = "first_name"
