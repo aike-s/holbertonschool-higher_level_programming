a68978b47b4a3e33ce309fae75cb4485c4ee616b
aike-s <sagrilo804@gmail.com>
2021-07-12 23:55:21 -0500

    [add] mysql scripts

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,6 +1,4 @@
 -- computes the score average of all records
 -- in the table second_table
-ALTER TABLE second_table ADD average;
-SELECT AVG(score) AS [score_average]
+SELECT AVG(score) AS average
 FROM second_table;
-INSERT INTO `second_table` (average) VALUES ([score_average]);
