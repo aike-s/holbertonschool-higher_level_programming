b0fec7fc372fc0b0875c82e018369caf42a52e04
aike-s <sagrilo804@gmail.com>
2021-07-12 22:14:44 -0500

    [fix] problems with scripts

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,6 @@
 -- creates a table second_table
 -- in the database hbtn_0c_0
-CREATE DATABASE IF NOT EXISTS second_table;
-CREATE TABLE second_table (id INT,
+CREATE TABLE IF NOT EXISTS second_table (id INT,
 name VARCHAR(256),
 score INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
