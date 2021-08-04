a26959ba1e4d756f17d287d9cf49475cafab4ccb
aike-s <sagrilo804@gmail.com>
2021-08-03 20:01:46 -0500

    [fix] semistandard style

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,10 +2,9 @@
 const { dict } = require('./101-data');
 const newDict = {};
 
-for(let key in dict) {
+for (const key in dict) {
   const value = dict[key];
-  newDict[value];
-  if(newDict[value] === undefined) {
+  if (newDict[value] === undefined) {
     newDict[value] = [];
   }
   newDict[value].push(key);
